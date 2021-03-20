6f89e245b96518da2383a5516fbffad2e4a7a27d
Bo Rydberg <2945606+bolry@users.noreply.github.com>
2021-03-20 23:06:26 +0100

    Add headers to main.cpp to test PCH

--- src/main.cpp
+++ src/main.cpp
@@ -20,4 +20,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include <vector>
+#include <string>
+#include <map>
+#include <utility>
+
 int main() {}
